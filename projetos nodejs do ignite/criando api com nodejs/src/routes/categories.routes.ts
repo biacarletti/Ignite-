815329c2c571614414ba14afaea0e16e1b436599
biacarletti <bia_carletti@hotmail.com>
2022-12-14 17:59:58 -0300

    realizando o principio de responsabilidade unica e principio de inversão de dependencia

--- projetos nodejs do ignite/criando api com nodejs/src/routes/categories.routes.ts
+++ projetos nodejs do ignite/criando api com nodejs/src/routes/categories.routes.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
+import { CreateCategoryService } from '../services/CreateCategoryService';
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
+
 
 
 const categoriesRoutes = Router();
@@ -8,14 +10,8 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
 
-    const categoryAlreadyExists = categoriesRepository.findByName(name);
-
-    if(categoryAlreadyExists){
-        return response.status(400).json({error: "Category Already Exists"})
-    }
-
-    categoriesRepository.create({name, description})
-    
+    const createCategoryService = new CreateCategoryService(categoriesRepository);
+    createCategoryService.execute({name, description})
     // 201 criado
     return response.status(201).send();
 });
